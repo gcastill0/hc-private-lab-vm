c165edcd9c0324eee833738c3b3863b76aa94e56
gcastill0 <gilberto@hashicorp.com>
2019-07-16 17:52:51 -0400

    Adding counts for VM

--- main.tf
+++ main.tf
@@ -39,11 +39,10 @@ resource "azurerm_virtual_machine" "main" {
   vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
+  delete_os_disk_on_termination = true
 
   # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
@@ -51,20 +50,24 @@ resource "azurerm_virtual_machine" "main" {
     sku       = "16.04-LTS"
     version   = "latest"
   }
+
   storage_os_disk {
     name              = "DISK-${count.index + 1}-${data.terraform_remote_state.azure_master.postfix}"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
+
   os_profile {
     computer_name  = "VM-${count.index + 1}-${data.terraform_remote_state.azure_master.postfix}"
     admin_username = "testadmin"
     admin_password = "Password1234!"
   }
+
   os_profile_linux_config {
     disable_password_authentication = false
   }
+
   tags = {
     environment = "Staging"
   }
