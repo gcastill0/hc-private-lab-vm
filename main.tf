22d30c59f972972289d1de9e7a88bef3ee57af5f
gcastill0 <gilberto@hashicorp.com>
2019-07-17 12:50:32 -0400

    Organized variables

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-variable "count" {
-  default = 2
-}
-
-variable "tfe_organization" {
-  default = "gcastill0"
-}
-
-variable "tfe_workspace" {
-  default = "azure-environment"
-}
-
 data "terraform_remote_state" "azure_master" {
   backend = "atlas"
 
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "count" {
+  default = 2
+}
+
+variable "tfe_organization" {
+  default = "gcastill0"
+}
+
+variable "tfe_workspace" {
+  default = "azure-environment"
+}
